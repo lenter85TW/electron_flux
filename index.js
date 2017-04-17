ba7f630030bbff86151227af0e7c1dc78ab6c5a5
KIM DOEUN <lenter85@twinny.co.kr>
2017-04-17 17:00:14 +0900

    add Browser create, delete control function.

--- index.js
+++ index.js
@@ -30,6 +30,42 @@ var mainDispatcher = {
     }
 };
 
+var mainWindowCRUD = {
+    init: function init(mainIpc, BrowserWindow) {
+        ipcMain = mainIpc;
+
+        ipcMain.on('createNewWindow', function (event, newWindowName, informationDataObj, htmlFileUrl) {
+            console.log("createNewWindow 실행", newWindowName, informationDataObj, htmlFileUrl);
+            var newBrowser = new BrowserWindow({width: informationDataObj.width, height: informationDataObj.height, frameless:true});
+
+            newBrowser.on('closed', () => {
+                console.log('window closed event receive');
+            mainProcess.removeWindow(newWindowName);
+            newBrowser = null;
+        })
+
+            newBrowser.loadURL(htmlFileUrl);
+            mainProcess.addWindow(newWindowName, newBrowser);
+
+        });
+
+        ipcMain.on('closeWindow', function (event, windowNameToClose) {
+            console.log("windowNameToClose 실행", windowNameToClose);
+
+            //일단 종료시킬 윈도우를 맵에서 가져오고
+            var browser = mainWindowListMap.get(windowNameToClose);
+
+            //윈도우 리스트 저장맵에서 삭제시켜준후
+            mainProcess.removeWindow(windowNameToClose);
+
+            //윈도우 종료.
+            browser.close();
+        });
+
+    }
+};
+
+
 var rendererAction = {
     init: function init(remdererIpc) {
         ipcRenderer = remdererIpc;
@@ -37,7 +73,19 @@ var rendererAction = {
     updateStore: function updateStore(dataName, data) {
         //console.log("rendereAction - updateStore", dataName, data);
         ipcRenderer.send('updateStore', dataName, data);
+    },
+
+    createNewWindow: function createNewWindow(newWindowName, informationDataObj, htmlFileUrl) {
+        console.log('createNewWindow 실행');
+        ipcRenderer.send('createNewWindow', newWindowName, informationDataObj, htmlFileUrl);
+    },
+
+    closeWindow: function closeWindow(windowNameToClose) {
+        console.log('closeWindow 실행');
+        ipcRenderer.send('closeWindow', windowNameToClose);
     }
+
+
 };
 
 var mainProcess = {
@@ -47,38 +95,26 @@ var mainProcess = {
         mainWindowListMap = windowListMap;
     },
 
-    // addWindow: function addWindow(winObj) {
-    //     windowList.push(winObj);
-    // },
+
 
     //////
     addWindow : function addWindow(key, windowObj){
         mainWindowListMap.set(key, windowObj);
     },
 
-    // removeWindow: function removeWindow(winObj) {
-    //     var index = windowList.indexOf(winObj);
-    //     windowList.splice(index, 1);
-    // },
 
     //////
     removeWindow: function removeWindow(key) {
         mainWindowListMap.delete(key);
     },
 
-    // changeData: function changeData(dataName, newData) {
-    //     //console.log("mainStore - changeData 실행", dataName, newData);
-    //     mainStoreObj[dataName] = newData;
-    //     windowList.forEach(function (currentValue) {
-    //         currentValue.webContents.send('dataChanged', dataName);
-    //     });
-    // },
+
 
     changeData: function changData(dataName, newData){
-        console.log('electron_flux - changeData excute', dataName, newData);
+        //console.log('electron_flux - changeData excute', dataName, newData);
         mainStoreObj[dataName] = newData;
         mainWindowListMap.forEach(function (currentValue, key) {
-            console.log('electron_flux - changeData ForEach execute',  key, currentValue);
+            //console.log('electron_flux - changeData ForEach execute',  key, currentValue);
             currentValue.webContents.send('dataChanged', dataName);
         });
     }
@@ -93,6 +129,7 @@ var rendererProcess = {
         main = rendererRemoteMain;
         ipcRenderer = rendererIpc;
         mainStoreObj = main.getStore();
+        mainWindowListMap = main.getWindowListMap();
         ipcRenderer.on('dataChanged', this.DataChanged);
     },
     DataChanged: function DataChanged(event, dataName) {
@@ -113,12 +150,13 @@ function getMainStoreObj() {
 
 
 function getMainWindowListMap() {
-    return windowListMap;
+    return mainWindowListMap;
 }
 
 exports.mainDispatcher = mainDispatcher;
 exports.rendererAction = rendererAction;
 exports.mainProcess = mainProcess;
 exports.rendererProcess = rendererProcess;
+exports.mainWindowCRUD = mainWindowCRUD;
 exports.getMainStoreObj = getMainStoreObj;
 exports.getMainWindowListMap = getMainWindowListMap;
